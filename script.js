68e765481f46a198fefae55a8c2a52ee7fd04934
Marius <105146774+Shedou85@users.noreply.github.com>
2022-09-20 14:56:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 
-const dollarInEuro = 0.99;
-const rublesInDollar = 60;
 
-function exchangeRate(euro, rubles) {
- 
-  let euroResult = euro * dollarInEuro;
-  let rublesResult = rubles * rublesInDollar;
-  console.log(euroResult > rublesResult);   
-  return `${euroResult} Euro\n${rublesResult} Rubles`;
+
+function myFn(dollar, rubles) {
+  return dollar >= rubles;
 }
-console.log(exchangeRate(100, 100))
+console.log(myFn(10, 10))
+
+function myFn2() {
+  let a = 100;
+  let b = 52;
+  return a + b
+}
+console.log(myFn2())
+
+
+function dollarConvert(dollar, euro) {
+  
+  return dollar * euro;
+}
+let result = dollarConvert(10, 5)
+console.log(result + ' Dollar')
+
+
+
+
+//const dollarInEuro = 0.99;
+//const rublesInDollar = 60;
+
+//function exchangeRate(euro, rubles) {
+ 
+//  let euroResult = euro * dollarInEuro;
+//  let rublesResult = rubles * rublesInDollar;
+//  console.log(euroResult > rublesResult);   
+//  return `${euroResult} Euro\n${rublesResult} Rubles`;
+//}
+//console.log(exchangeRate(100, 100))
