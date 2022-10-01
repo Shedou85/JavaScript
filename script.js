9ce1c60699862a8c0efb1259079f340ae5037f98
Marius <105146774+Shedou85@users.noreply.github.com>
2022-10-01 08:54:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let a = '12.4';
-let b = "36";
+let b = 36;
 let c = '90';
 
 function myFn (a, b, c,) {
@@ -11,14 +11,19 @@ function myFn (a, b, c,) {
        else  ( 
          a == String,
          b == String,
-         c == String)
+         c == String);
         {
          a = +a;
          b = +b;
          c = +c;
-         console.log( typeof b + ' Proverka preobrazovanyje tipov')
-        }
-   
-   return a * b * c;
+         //console.log( typeof b + ' Proverka preobrazovanyje tipov')
+        };
+   let sum = a * b * c;
+   return  sum;
+    
 }
-console.log(myFn(a, b, c))
+console.log(myFn(a, b, c));
+
+let sum = myFn(a, b, c);
+console.log(typeof sum);
+console.log('Suma vycheslenyje ' + sum)
