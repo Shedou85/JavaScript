75a60a7fbbfadccb018d405b3a90b15844ac1311
Marius <105146774+Shedou85@users.noreply.github.com>
2022-10-01 14:33:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-let a = '12.4';
-let b = 36;
-let c = '90';
-
-function myFn (a, b, c,) {
-   if (a == Number,
-      b == Number,
-      c == Number
-      );
-      
-       else  ( 
-         a == String,
-         b == String,
-         c == String);
-        {
-         a = +a;
-         b = +b;
-         c = +c;
-         //console.log( typeof b + ' Proverka preobrazovanyje tipov')
-        };
-   let sum = a * b * c;
-   return  sum;
-    
+let a = '2';
+let b = '2';
+let c = '2';
+//console.log(typeof a) // sostojanyje peremeny
+function myFn(a, b, c) {
+   a = Number(a);
+   b = Number(b);
+   c = Number(c);
+   //console.log(typeof a) // preobrazovanyje stroki v number
+   return a * b * c
 }
-console.log(myFn(a, b, c));
 
-let sum = myFn(a, b, c);
-console.log(typeof sum);
-console.log('Suma vycheslenyje ' + sum)
+
+let total = myFn(a, b, c)
+console.log(total)
+//console.log(typeof total) // proverka rezultata v chislo
+
