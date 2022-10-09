ed7c3c4e2a4245667014a56c9854e67ae9f6fc61
Marius <105146774+Shedou85@users.noreply.github.com>
2022-10-09 19:05:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,63 @@
 
 let arr = [
    {
-      imgSrc: "../img/flower1.webp",
+      imgSrc: "./img/flower1.webp",
       flowerName: "Rose & Lily",
       description: "With classic floral colors including red roses and pink lilies.",
       flowerClass: "Rose",
       price: "$35.99"
    },
    {
-      imgSrc: "../img/flower2.webp",
+      imgSrc: "./img/flower2.webp",
       flowerName: "Sunflower Surprise",
       description: "Green poms, green hypericum berries and green trachelium surround the sunflowers.",
       flowerClass: "Mason Jar",
       price: "$35.99"
    },
    {
-      imgSrc: "../img/flower3.webp",
+      imgSrc: "./img/flower3.webp",
       flowerName: "Harvest Pumpkin",
       description: "Nothing says Fall like pumpkins, so brighten someone's day.",
       flowerClass: "Mason Jar",
       price: "$40.99"
    },
    {
-      imgSrc: "../img/flower4.webp",
+      imgSrc: "./img/flower4.webp",
       flowerName: "Purple Melody",
       description: "Purple and perfect, these sophisticated flowers will speak.",
       flowerClass: "Purple Roses",
       price: "$35.99"
    },
    {
-      imgSrc: "../img/flower5.webp",
+      imgSrc: "./img/flower5.webp",
       flowerName: "Daisy & Rose",
       description: "Delight someone special in your life, or treat yourself.",
       flowerClass: "White Daisies",
       price: "$39.99"
    },
    {
-      imgSrc: "../img/flower6.webp",
+      imgSrc: "./img/flower6.webp",
       flowerName: "Perfect Love Bouquet",
       description: "Love is in the air and this Valentine's Day flower delivery.",
       flowerClass: "Red Carnations",
       price: "$69.99"
    },
    {
-      imgSrc: "../img/flower7.webp",
+      imgSrc: "./img/flower7.webp",
       flowerName: "The Sunrise Bouquet",
       description: "Gushing with vibrant colors, this unique flower arrangement.",
       flowerClass: "Red Carnations",
       price: "$55.99"
    },
    {
-      imgSrc: "../img/flower8.webp",
+      imgSrc: "./img/flower8.webp",
       flowerName: "Fiery Autumn Glow",
       description: "Send them festive fall wishes with this stunning fresh bouquet of roses.",
       flowerClass: "Red Carnations",
       price: "$51.99"
    },
    {
-      imgSrc: "../img/flower9.webp",
+      imgSrc: "./img/flower9.webp",
       flowerName: "Rose & Lily",
       description: "With classic floral colors including red roses and pink lilies.",
       flowerClass: "Rose",
